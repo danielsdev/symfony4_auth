128a3c51cf68b026cf4ed3b509c4fde4c7b9f384
Daniel Lima <danielrlima2015@gmail.com>
2019-06-30 00:14:58 -0300

    Usando Access Control para bloquear rotas

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    public function admin(){
+
+        return new Response("<html><body><h1>Page Admin! </h1></body></html>");
+    }
+}
